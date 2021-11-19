7c1c6bcfed673d545e3c5100427f60976b6f4ac1
DArmstrong87 <lenzflare@gmail.com>
2021-11-19 15:52:45 -0600

    corrected sales by year

--- total_sales_{year}.sql
+++ total_sales_{year}.sql
@@ -5,3 +5,10 @@ strftime('%Y', InvoiceDate) as Year,
 sum(total) as InvoiceCount
 FROM Invoice
 GROUP BY Year;
+
+SELECT Distinct
+strftime('%Y', InvoiceDate) as Year,
+sum(total) as InvoiceCount
+FROM Invoice
+WHERE Year = "2009" or  Year = "2011"
+GROUP BY Year;
